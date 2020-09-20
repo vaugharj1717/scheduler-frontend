97ce52da17a42a6c43853dd55a5cc2a6482e1f8f
FANGH <FANGH8768@uwec.edu>
2020-09-20 06:50:58 +0200

    (CAN-17) Frontend: Access endpoint appropriately. Implement search on returned JSON

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ class App extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            positions: []
+            positions: [],
+            participants: []
         }
     }
     getPositionsByDepartement(id){
         let url ="http://localhost:8444/position/department/";
         url = url + id + '/position';
         fetch(url,{method: "GET"})
+            .then(res => res.json())
             .then(data =>{
                 this.setState({
                     positions: data
+                });
+            })
+    }
+    getAllParticipants(){
+        let url ="http://localhost:8444/user/participant/";
+        fetch(url,{method: "GET"})
+            .then(res => res.json())
+            .then(data =>{
+                this.setState({
+                    participants: data
                 });
             })
     }
@@ -22,6 +34,7 @@ class App extends React.Component{
       return (
           <div>
             <button onClick={()=>this.getPositionsByDepartement(1)}>getPositionsByDepartement</button>
+              <button onClick={()=>this.getAllParticipants()}>getAllParticipants</button>
             <div>Hello, world!</div>
           </div>
         );

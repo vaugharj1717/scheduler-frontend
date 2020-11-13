771ce1554c4dd28dbc9d451f84d3d249d4a98f47
vaugharj1717 <vaugharj1717@gmail.com>
2020-11-12 18:42:52 -0600

    useState example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Switch, Route, Link, Redirect} from 'react-router-dom';
-import Test from './test/Test.js';
+// import Test from './test/Test.js';
 import LoginPage from './login-page/Login-Page.js';
 import {beginLoggingOut} from './actions.js'
 import {useSelector, useDispatch} from 'react-redux';
@@ -9,7 +9,10 @@ import {useSelector, useDispatch} from 'react-redux';
 function App() {
   const dispatch = useDispatch();
   let currentUser = useSelector(state => state.currentUser)
-  
+  //let [creatingCandidate, setCreatingCandidate] = useState(false);
+  //if new state depends on old state, pass function into it (toggle)
+  //setCreatingCandidate(creatingCandidate => !creatingCandidate)
+
   {/* If user is not logged in... */}
   if(currentUser == null){
     return(

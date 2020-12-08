4aa107a35fd42f2305a46e42e5b750a74ababc25
vaugharj1717 <vaugharj1717@gmail.com>
2020-12-08 12:11:16 -0600

    Fix DepartmentAdmin file upload

--- src/test/file-pane/File-Pane.js
+++ src/test/file-pane/File-Pane.js
@@ -38,7 +38,7 @@ function FilePane(props) {
             <div onClick={()=> dispatch(setIsViewingFiles(false))} id="file-pane-exit">X</div>
             <div id="file-pane-container">
                 <div onClick={handleGoToUserInfo} className="files-user-info-link"><span>&#171;</span> Back</div>
-                {(currentUser.id === userId || currentUser.role === 'SCHEDULER' || currentUser.role === 'ADMIN' || currentUser.role === 'SUPER_ADMIN' || (currentUser.role === 'DEPARTMENT_ADMIN' && currentUser.department !== null && currentUser.department.id === userId)) &&
+                {(currentUser.id === userId || currentUser.role === 'SCHEDULER' || currentUser.role === 'ADMIN' || currentUser.role === 'SUPER_ADMIN' || currentUser.role === 'DEPARTMENT_ADMIN') &&
                 <div className="add-file-button">
                     <input onChange={(e)=>{handleFileUpload(userId, e)}}type="file" id="upload-file"></input>
                     <label htmlFor="upload-file" className="upload-file-label">Add File</label>

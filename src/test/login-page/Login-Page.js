344a7389f0dd86c57121706c902ab8bae327194c
vaugharj1717 <vaugharj1717@gmail.com>
2021-01-12 16:06:07 -0600

    Added tooltip to login screen for presentation purposes with some default login credentials

--- src/test/login-page/Login-Page.js
+++ src/test/login-page/Login-Page.js
@@ -22,6 +22,21 @@ export default function LoginPage(props){
                 <div id="login-header">UWEC Scheduler</div>
                 <div id="login-form-container">
                     <div id="login-form-header">LOGIN</div>
+                    <div id="login-tooltip-btn">
+                        i
+                        <div id="login-tooltip-txt">
+                            This system does not allow self-registering. Registration must be done by a system administrator.
+                            For development/presentation purposes, here are some credentials that can be used.
+                            <ul id="login-tooltip-credentials">
+                                <li><pre>participant@gmail.com     PASS: participant</pre></li>
+                                <li><pre>candidate@gmail.com       PASS: candidate</pre></li>
+                                <li><pre>scheduler@gmail.com       PASS: scheduler</pre></li>
+                                <li><pre>admin@gmail.com           PASS: admin</pre></li>
+                                <li><pre>superadmin@gmail.com      PASS: superadmin</pre></li>
+                                <li><pre>departmentadmin@gmail.com PASS: departmentadmin</pre></li>
+                            </ul>
+                        </div>
+                    </div>
                     <div className="login-form-input-grp">
                         <label className="login-label">Email: </label>
                         <input className="login-input" type="textbox" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
